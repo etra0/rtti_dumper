etra0/rtti_dumper

commit cafb5e0206ea394208501f45ba149994ab03a105

Removed unnecesary function
Removed `get_module_name` since it's already present on memory-rs.

src/lib.rs
@@ -1,4 +1,3 @@
-use globals::Parameters;
 use memory_rs::internal::memory::resolve_module_path;
 use memory_rs::internal::process_info::ProcessInfo;
 use memory_rs::try_winapi;
@@ -104,20 +103,6 @@ unsafe extern "system" fn wrapper(lib: LPVOID) -> u32 {
     0
 }
 
-unsafe fn get_module_name(lib: LPVOID) -> Result<String> {
-    let mut buf: Vec<i8> = Vec::with_capacity(255);
-
-    try_winapi!(libloaderapi::GetModuleFileNameA(
-        lib as _,
-        buf.as_mut_ptr(),
-        255
-    ));
-    let name = CStr::from_ptr(buf.as_ptr());
-    let name = String::from(name.to_str()?);
-
-    Ok(name)
-}
-
 fn get_rtti_values(lib: LPVOID, params: globals::Parameters) -> Result<&'static str> {
     let proc_inf = ProcessInfo::new(None)?;
 
@@ -132,7 +117,7 @@ fn get_rtti_values(lib: LPVOID, params: globals::Parameters) -> Result<&'static 
     let av_matches = region.scan_aob_all_matches(av_signature_lambda, 4)?;
 
     let game_name: String = unsafe {
-        let name: PathBuf = get_module_name(std::ptr::null_mut())?.into();
+        let name: PathBuf = resolve_module_path(std::ptr::null_mut())?;
         let name = name.file_name().ok_or("Couldn't get exec name")?;
         String::from(name.to_string_lossy())
     };

commit 40de435104e8d49a33e26ec54cc520b55bc4b324

hotfix: game_name now is correct
Regression from cafb5e0206ea394208501f45ba149994ab03a105 fixed.

src/lib.rs
@@ -7,7 +7,7 @@ use std::path::PathBuf;
 use std::sync::{atomic::AtomicUsize, atomic::Ordering, Arc};
 use winapi::shared::minwindef::LPVOID;
 use winapi::um::consoleapi::AllocConsole;
-use winapi::um::libloaderapi::FreeLibraryAndExitThread;
+use winapi::um::libloaderapi::{self, FreeLibraryAndExitThread};
 use winapi::um::wincon::FreeConsole;
 use winapi::um::winuser::MessageBoxA;
 
@@ -80,6 +80,20 @@ unsafe extern "system" fn wrapper(lib: LPVOID) -> u32 {
     0
 }
 
+unsafe fn get_module_name(lib: LPVOID) -> Result<String> {
+    let mut buf = [0_u8; 255];
+
+    memory_rs::try_winapi!(libloaderapi::GetModuleFileNameA(
+        lib as _,
+        buf.as_mut_ptr() as *mut i8,
+        255
+    ));
+    let null_terminator = buf.iter().position(|&x| x == 0).unwrap();
+    let name = String::from_utf8_lossy(&buf[..null_terminator]).to_string();
+
+    Ok(name)
+}
+
 fn get_rtti_values(lib: LPVOID, params: globals::Parameters) -> Result<&'static str> {
     let proc_inf = ProcessInfo::new(None)?;
 
@@ -94,7 +108,7 @@ fn get_rtti_values(lib: LPVOID, params: globals::Parameters) -> Result<&'static 
     let av_matches = region.scan_aob_all_matches(av_signature_lambda, 4)?;
 
     let game_name: String = unsafe {
-        let name: PathBuf = resolve_module_path(std::ptr::null_mut())?;
+        let name: PathBuf = get_module_name(std::ptr::null_mut())?.into();
         let name = name.file_name().ok_or("Couldn't get exec name")?;
         String::from(name.to_string_lossy())
     };